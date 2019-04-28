ca702e3b170682ac9d7c0a7620f8be1cd1a5c1e1
Daniel <danisan@stud.ntnu.no>
2019-04-28 07:05:27 +0200

    Updates

---
+++ precache-manifest.96aa51fd41c5a26642e892b022fa9abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d104cbdc4324d5dd72",
+    "url": "/smart-brain/static/js/runtime~main.c0d104cb.js"
+  },
+  {
+    "revision": "f92263803f9d2f7fd04d",
+    "url": "/smart-brain/static/js/main.f9226380.chunk.js"
+  },
+  {
+    "revision": "49202a2d5af10cfb7277",
+    "url": "/smart-brain/static/js/2.49202a2d.chunk.js"
+  },
+  {
+    "revision": "f92263803f9d2f7fd04d",
+    "url": "/smart-brain/static/css/main.8938cad1.chunk.css"
+  },
+  {
+    "revision": "49202a2d5af10cfb7277",
+    "url": "/smart-brain/static/css/2.0495ea46.chunk.css"
+  },
+  {
+    "revision": "6f70d588d6ec8feb5e359e1fabaabc55",
+    "url": "/smart-brain/index.html"
+  }
+];
